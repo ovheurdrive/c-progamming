750b47ba4e7a2c2ae9ea33356931fc0cd9b6b6a2
Overdrive <stanislas.plessia@student.ecp.fr>
2016-12-20 00:14:29 +0100

    Basics : types.c : Add different types

--- Basics/types.c
+++ Basics/types.c
@@ -1,17 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(char* argv, int argc)
-{
-	printf("Hello World!");
-	return 0;
-}
-
+void y; // type "vide", peut contenir un peu tout ce que tu veux
+short z; // entier sur 16 bits
 int a; // entier sur 16 bits
-long b; // entier sur 32 bits
-uint64_t c; // uint64_t = unsigned long long  ----> entier non signé sur 64 bits
+long b; // entier sur 32 bits (long == long int , abréviation pour raccourcir)
+uint64_t c; // uint64_t = unsigned long long int  ----> entier non signé sur 64 bits
 
 // Définir un type :
 typedef long long long int kiki; // kiki est un type d'entier sur 128 bits
-kiki a; // a est une variable de type kiki
+kiki d; // a est une variable de type kiki
 
+// Type des nombres réels (dits à virgule flottante)
+float e;
+double f;
+long double g;
+
+// Chaine de charactère/Tableaux
+char h;
+char i[200];
+
+// Structures
+struct j
+{
+	int k;
+	char l;
+}
+
+// Enum : associer une chaine de charactère à un nombre de manière identifié
+enum m
+{
+	N,
+	O
+};
+
+bool p; // variable booléenne
