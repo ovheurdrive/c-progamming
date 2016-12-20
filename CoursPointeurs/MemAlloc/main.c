133c88ce9fa811312563ce2498322a841fe92a03
Overdrive <stanislas.plessia@student.ecp.fr>
2016-12-20 16:31:37 +0100

    Pointers : MemAlloc : add comments

--- CoursPointeurs/MemAlloc/main.c
+++ CoursPointeurs/MemAlloc/main.c
@@ -33,7 +33,16 @@ int empty(table_t* table)
 int main(char* argv, int argc)
 {
     table_t* table = NULL;
-    table = calloc(sizeof(*table),1);
+    table = calloc(sizeof(*table),1); // calloc = allocate mémory and set it to 0
+	/*
+	 * Function to allocate memory :
+	 * ptr = malloc(size) : ptr is a pointer to an allocated chunk of size "size"
+	 * ptr = calloc(size,1) : same but memory is set to zero
+	 * free(ptr) : the memory allocated at ptr is freed (unallocated)
+	 *
+	 * There are other functions that allocate memory :
+	 * asprintf, strdup ... but this is the basics.
+	 */ 
 
     filling(table);
     printf("%d, %f, %s\n", table->i, table->f, table->s); 
