c91bd8d3cb011ab6bd27f6688b371ead1b8baa55
Overdrive <stanislas.plessia@student.ecp.fr>
2016-12-20 15:49:36 +0100

    Basics : I/O : add file opening and binary

--- Basics/basic_io.c
+++ Basics/basic_io.c
@@ -1,11 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define CHUNK 2048
 
+int file_open(const char* filename)
+{
+	FILE* p_file = fopen(filename,"r");
+	char buff[CHUNK];
+	size_t size_read;
+	if(p_file == NULL)
+	{
+		printf("Error opening file");
+		fclose(p_file);
+	}
+	else
+	{
+		while( size_read = fread(buff,1,sizeof(buff),p_file) > 0)
+			printf("%s",buff);
+		fclose(p_file);
+	}
+	return 0;
+}
 int main(char* argv, int argc)
 {
 	int a;
-	scanf("%d", a); // permet de prendre la valeur de a dans l'entrée standard (stdin) de manière formatée
-	printf("%d", a); // affiche la valeur de a dans la sortie standard (stdout) de manière formatée
-
+	scanf("%d", &a); // permet de prendre la valeur de a dans l'entrée standard (stdin) de manière formatée
+	printf("%d\n", a); // affiche la valeur de a dans la sortie standard (stdout) de manière formatée
+	
+	file_open("file_to_open.txt");
+	
 	return 0;
+
 }
