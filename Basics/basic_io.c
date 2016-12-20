351cb37a7d84abd3a409d98e5827731e183616e1
Overdrive <stanislas.plessia@student.ecp.fr>
2016-12-20 15:05:14 +0100

    Basics : add basic explanation for I/O

---
+++ Basics/basic_io.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(char* argv, int argc)
+{
+	int a;
+	scanf("%d", a); // permet de prendre la valeur de a dans l'entrée standard (stdin) de manière formatée
+	printf("%d", a); // affiche la valeur de a dans la sortie standard (stdout) de manière formatée
+
+	return 0;
+}
