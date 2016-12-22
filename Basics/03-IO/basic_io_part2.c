1c4fd5d242e2564e74dcbce666c3de57b724c6cd
Overdrive <stanislas.plessia@student.ecp.fr>
2016-12-22 18:48:18 +0100

    Code : IO part 2 : Add comment

--- Basics/03-IO/basic_io_part2.c
+++ Basics/03-IO/basic_io_part2.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 #define CHUNK 2048
 
+// For file handling, there are I/O functions associated to any existing
+// basic I/O function with an 'f' in front of t :
+//
+// fprintf, fscanf, fgets, fgetc, fputs, fputc, fread, fwrite.
+//
+// There are also functions to handle the file itself and cursors inside of it :
+//
+// fopen, fclose, fseek, fflush...
+
 int file_open(const char* filename)
 {
 	FILE* p_file = fopen(filename,"r");
